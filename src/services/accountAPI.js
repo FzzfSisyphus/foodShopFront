e625b28e14ad20bc32a1961a3a0064b7e5767825
lanping <904315628@qq.com>
2023-10-31 23:22:24 +0800

    change vue.config

--- src/services/accountAPI.js
+++ src/services/accountAPI.js
@@ -2,14 +2,14 @@ import API from "./API";
 
 class accountAPI {
     static login(userName, password) {
-        return API('http://localhost:8001/consumers/user/basic-auth').post('/login', {
+        return API('http://ec2-54-254-59-196.ap-southeast-1.compute.amazonaws.com:8001/consumers/user/basic-auth').post('/login', {
             userName: userName,
             password: password
         })
     }
 
     static signup(formData) {
-        return API('http://localhost:8001/consumers/user').post('/basic-auth',formData)
+        return API('http://ec2-54-254-59-196.ap-southeast-1.compute.amazonaws.com:8001/consumers/user').post('/basic-auth',formData)
     }
 }
 
